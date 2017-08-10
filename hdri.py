394bc9958ca25af7e0b33e828c74ee1ed8cf8174
Stiliana <stiliana-g@outposvfx.co.uk>
2017-08-10 16:01:51 +0100

    hdri test

---
+++ hdri.py
@@ -0,0 +1,18 @@
+import pymel.core as pm
+
+_path = ""
+_frame = ""
+pm.mel.eval('redshiftCreateDomeLight;')
+pm.mel.eval('MASHnewNodeCallback( "rsDomeLightShape1");')
+pm.mel.eval('setAttr -type "string" rsDomeLightShape1.tex0 "{0}";'.format(_path))
+
+
+# set 0 keyframe rotation
+pm.mel.eval('currentTime 1;')
+pm.mel.eval('setAttr "rsDomeLight1.rotateY 0";')
+pm.mel.eval('setKeyframe { "rsDomeLight1.r" };')
+
+#set user keyframe rotation
+pm.mel.eval('currentTime 1;')
+pm.mel.eval('setAttr "rsDomeLight1.rotateY {0}";'.format(_frame))
+pm.mel.eval('setKeyframe { "rsDomeLight1.r" };')
